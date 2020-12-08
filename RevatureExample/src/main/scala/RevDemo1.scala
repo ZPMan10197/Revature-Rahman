034330d22bca9ef5a9c009305ae103a9600497b4
Zeshawn Manzoor <zmanzoor@umd.edu>
2020-12-08 16:18:28 -0500

    first try commit

---
+++ RevatureExample/src/main/scala/RevDemo1.scala
@@ -0,0 +1,35 @@
+
+  import java.io.PrintWriter
+
+  object RevDemo1 {
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    def simpleArraySum(ar: Array[Int]): Int = {
+      var res  = 0;
+      var i = 0;
+      while(i < ar.length){
+        res += ar(i);
+        i += 1;
+      }
+      return res;
+    }
+
+    def main(args: Array[String]) {
+      val stdin = scala.io.StdIn
+
+      val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+      val arCount = stdin.readLine.trim.toInt
+
+      val ar = stdin.readLine.split(" ").map(_.trim.toInt)
+      val result = simpleArraySum(ar)
+
+      printWriter.println(result)
+
+      printWriter.close()
+    }
+  }
+
+
