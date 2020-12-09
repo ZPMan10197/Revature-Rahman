38725c4de7a411d824d73c6955e61d0a5582c6d1
Zeshawn Manzoor <zmanzoor@umd.edu>
2020-12-08 21:13:09 -0500

    update 3

--- RevatureExample/src/main/scala/RevDemo1.scala
+++ RevatureExample/src/main/scala/RevDemo1.scala
@@ -19,16 +19,17 @@
     def main(args: Array[String]) {
       val stdin = scala.io.StdIn
 
-      val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
 
       val arCount = stdin.readLine.trim.toInt
 
       val ar = stdin.readLine.split(" ").map(_.trim.toInt)
       val result = simpleArraySum(ar)
 
-      printWriter.println(result)
 
-      printWriter.close()
+      System.out.println(result)
+
+
     }
   }
 
